608273bd527eafa6803b4575faf9ecf7521783eb
HalukJ <senocakhaluk45@gmail.com>
2025-11-19 09:36:25 +0100

    user.py added

--- user.py
+++ user.py
@@ -1,2 +1,17 @@
-def dived (a,b):
+def divide (a,b):
     return a/b
+
+
+def scoreDived ():
+    print("Welcome to Calculator!")
+  
+
+
+
+    
+    x = int(input("First number: "))
+    y = int(input("Second number: "))
+    print("Result:", divide(x, y))
+
+if __name__ == "__main__":
+    scoreDived()
